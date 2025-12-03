f232d618a42c859939831bebe774a401b4728e46
Erick Tsafack <ericktsafack1aff@gmail.com>
2025-12-03 06:42:09 +0100

    reparation des encodages en utf8

--- donnees/connexion.php
+++ donnees/connexion.php
@@ -1,48 +1,49 @@
 <?php
 
 
-function connexion(){
-    try{
+function connexion()
+{
+    try {
         $bdd = new PDO('mysql:host=mysql-126762-0.cloudclusters.net;dbname=immo', 'root', '');
-//        $bdd = new PDO('mysql:host=localhost;dbname=rapelle', 'root', '');
+        //        $bdd = new PDO('mysql:host=localhost;dbname=rapelle', 'root', '');
         return $bdd;
-    }
-    catch (Exception $e){
+    } catch (Exception $e) {
         die('Erreur : ' . $e->getMessage());
     }
-       
+
 }
 
-class Connexion{
+class Connexion
+{
     static public $db_name = 'suivi_aep_fokoue';
     static public $db_user = 'root';
     static public $db_host = 'localhost';
     static public $db_password = '';
     static private $bdd = null;
-    public static function connect(){
-        if(self::$bdd != null)
+    public static function connect()
+    {
+        if (self::$bdd != null)
             return self::$bdd;
-        try{
+        try {
 
-//            new PDO("mysql:host=localhost;dbname=banane", "root", "");
-            $bdd = new PDO("mysql:host=".self::$db_host.";dbname=".self::$db_name, self::$db_user, self::$db_password, array(PDO::ATTR_PERSISTENT=>true));
-//            $bdd = new PDO('mysql:host=mysql-126762-0.cloudclusters.net;port=10015;dbname=immo', 'admin', 'vJ69WFrJ');
-            $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO:: ERRMODE_EXCEPTION);
+            //            new PDO("mysql:host=localhost;dbname=banane", "root", "");
+            $bdd = new PDO("mysql:host=" . self::$db_host . ";dbname=" . self::$db_name, self::$db_user, self::$db_password, array(PDO::ATTR_PERSISTENT => true));
+            //            $bdd = new PDO('mysql:host=mysql-126762-0.cloudclusters.net;port=10015;dbname=immo', 'admin', 'vJ69WFrJ');
+            $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $bdd->exec("SET NAMES utf8");
-            $bdd->exec("SET CHARACTER SET utf8");
+            // $bdd->exec("SET CHARACTER SET utf8");
             return $bdd;
-        }
-        catch (Exception $e){
+        } catch (Exception $e) {
             die('Erreur : ' . $e->getMessage());
         }
     }
-    public static function connectXml(){
-        try{
+    public static function connectXml()
+    {
+        try {
             //$bd = simplexml_load_file('../donnees/bd.tld');
             $bd = simplexml_load_file('donnees/bd.tld');
             return $bd;
-        }
-        catch (Exception $e){
+        } catch (Exception $e) {
             die('Erreur : ' . $e->getMessage());
         }
     }
@@ -50,4 +51,3 @@ class Connexion{
 
 
 Connexion::connect();
-    
