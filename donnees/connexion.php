bac63650546687edddc44b2bae3ce08804b7f1a7
Erick Tsafack <ericktsafack1aff@gmail.com>
2025-12-03 06:41:09 +0100

    reparation des encodages en utf8

--- donnees/connexion.php
+++ donnees/connexion.php
@@ -28,6 +28,8 @@ class Connexion{
             $bdd = new PDO("mysql:host=".self::$db_host.";dbname=".self::$db_name, self::$db_user, self::$db_password, array(PDO::ATTR_PERSISTENT=>true));
 //            $bdd = new PDO('mysql:host=mysql-126762-0.cloudclusters.net;port=10015;dbname=immo', 'admin', 'vJ69WFrJ');
             $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO:: ERRMODE_EXCEPTION);
+            $bdd->exec("SET NAMES utf8");
+            $bdd->exec("SET CHARACTER SET utf8");
             return $bdd;
         }
         catch (Exception $e){
